3fd0f5b9a5280133300e9656d022ed1b9bcb194e
madblocksgit <madblocks2k19@gmail.com>
2022-05-26 14:06:53 +0530

    yolov3 weights update

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 YOLOV4_LABELS_PATH='./yolov4/coco.names'
 YOLOV4_CFG_PATH='./yolov4/yolov4.cfg'
-YOLOV4_WEIGHTS_PATH='./yolov4/yolov4.weights'
+YOLOV4_WEIGHTS_PATH='../../yolov3.weights'
 
 OUTPUT_PATH='./output/output.avi'
 SAFE_DISTANCE= 300
